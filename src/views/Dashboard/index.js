65a6f93da14887c8a9ac2da1751c23ceda0ea38f
Fuad_Herac <fuad.herac@gmail.com>
2020-06-21 01:04:25 +0200

    Dodan prikaz email-a malo ubagovan

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -68,8 +68,7 @@ class Dashboard extends React.Component {
           <Header className="site-layout-background" style={{ padding: 0 }} />
           <Content style={{ margin: '0 16px' }}>
             <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+              <Breadcrumb.Item>User / {this.state.email}</Breadcrumb.Item>
             </Breadcrumb>
             <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
               Bill is a cat.
