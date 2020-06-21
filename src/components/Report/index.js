75076e403f76e9b85b86b3dde8dda25df99f760c
Fuad_Herac <fuad.herac@gmail.com>
2020-06-21 16:14:30 +0200

    Male izmjene

--- src/components/Report/index.js
+++ src/components/Report/index.js
@@ -6,7 +6,7 @@ const Report = (props) => {
     return (
         <>
             <h3>Report</h3>
-            <StyledDescriptions bordered column={1}>
+            <StyledDescriptions bordered size="medium" column={1}>
                 <Descriptions.Item label="User">{props.email}</Descriptions.Item>
                 <Descriptions.Item label="Hours focused">16 (placeholder)</Descriptions.Item>
                 <Descriptions.Item label="This week">2 (placeholder)</Descriptions.Item>
---
+++ src/views/Dashboard/style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { Layout } from 'antd';
+ 
+export const StyledLayout = styled(Layout)`
+    .ant-layout-sider-dark {
+        background-color: #f5222d;
+        color:red;
+    }
+`;
