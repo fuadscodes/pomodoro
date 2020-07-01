48dabf8ea9186c55029b948cc89a0b8bb08e637e
Fuad_Herac <fuad.herac@gmail.com>
2020-07-01 11:16:33 +0200

    Završei sati u Report-u. Završiti prvo thisMonth i thisYear pa onda preći na thisWeek.

--- src/components/Report/index.js
+++ src/components/Report/index.js
@@ -5,12 +5,25 @@ import axios from '../../config/axios-pomodoro';
 
 const Report = (props) => {
 
-    const [seconds, setSeconds] = useState(0);
+    // Prvo uraditi this month i this year i onda
+    const [hours, setHours] = useState(0);
+    const [thisWeek, setThisWeek] = useState(0);
+    const [thisMonth, setThisMonth] = useState(0);
+    const [thisYear, setThisYear] = useState(0);
 
-    // Get funkcija za povlačenje svih podataka iz baze i onda napisati logiku za razvrstavanje podataka
+    let tempSeconds = 0;
     axios.get('https://pomodoro-98f43.firebaseio.com/pomodoros.json')
     .then(response => {
-        console.log(JSON.stringify(response.data));
+        const temp = JSON.stringify(response.data).split('"');
+        console.log(JSON.stringify(response.data).split('"'));
+        for(let i = 0; i < temp.length; i+=12) {
+            if((JSON.stringify(response.data).split('"')[(i-1)]) === props.email) {
+                let sec = (JSON.stringify(response.data).split('"')[i-8]);
+                tempSeconds += parseInt(sec.replace(':', '').replace(',', ''))
+            }
+        }
+        console.log(tempSeconds);
+        setHours((tempSeconds/3600).toFixed(2));
     })
     .catch(error => {
         console.log(error);
@@ -22,10 +35,10 @@ const Report = (props) => {
             <h3>Report</h3>
             <StyledDescriptions bordered size="medium" column={1}>
                 <Descriptions.Item label="User">{props.email}</Descriptions.Item>
-                <Descriptions.Item label="Hours focused">16 (placeholder)</Descriptions.Item>
-                <Descriptions.Item label="This week">2 (placeholder)</Descriptions.Item>
-                <Descriptions.Item label="This month">16 (placeholder)</Descriptions.Item>
-                <Descriptions.Item label="This year">16 (placeholder)</Descriptions.Item>
+                <Descriptions.Item label="Hours focused">{hours}</Descriptions.Item>
+                <Descriptions.Item label="This week">{thisWeek}</Descriptions.Item>
+                <Descriptions.Item label="This month">{thisMonth}</Descriptions.Item>
+                <Descriptions.Item label="This year">{thisYear}</Descriptions.Item>
             </StyledDescriptions>
         </>
     );
